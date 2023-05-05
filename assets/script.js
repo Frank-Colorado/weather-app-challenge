89f2e7fe790365c2a68ae97190b053dc86e1baa0
Frank-Colorado <chawnkiee@gmail.com>
2023-05-04 22:19:23 -0400

    refactored 'getWeatherData' async function

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,37 @@
 // API KEY VARIABLE
 const API_KEY = "c6bc260f8c642aa6780da0d9f526c867";
+const WEATHERAPI =
+  "https://api.openweathermap.org/data/2.5/weather?q=&appid=c6bc260f8c642aa6780da0d9f526c867";
+const FORECASTAPI =
+  "https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c6bc260f8c642aa6780da0d9f526c867";
 // SIDEBAR VARIABLES
 const searchInput = document.getElementById("city-input");
 // CURRENT WEATHER DISPLAY VARIABLES
-const cityName = getElementById("city-name");
-const date = getElementById("date");
-const weatherIcon = getElementById("weather-type");
-const temp = getElementById("temp");
-const humidity = getElementById("humidity");
-const wind = getElementById("wind");
+const cityName = document.getElementById("city-name");
+const date = document.getElementById("date");
+const weatherIcon = document.getElementById("weather-type");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
 
 const getWeatherData = async () => {
   try {
     const city = searchInput.value || "Los Angeles";
-
-    const currentWeather = new Promise(async (resolve, reject) => {
-      try {
-        const weatherApiData = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-        );
-        resolve(await weatherApiData.json());
-      } catch (err) {
-        reject();
-      }
-    });
-
-    const data = await currentWeather;
-    currentWeatherDisplay(data);
-  } catch (err) {
-    console.log("Error", err);
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6bc260f8c642aa6780da0d9f526c867`;
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c6bc260f8c642aa6780da0d9f526c867`;
+    const data = await Promise.all([
+      getApiData(currentWeatherURL),
+      getApiData(forecastURL),
+    ]);
+    console.log(data);
+    // currentWeatherDisplay(data);
+  } catch (error) {
+    console.log("Error", error);
   }
 };
 
+//
+const currentWeatherDisplay = (data) => {
+  cityName.innerText = data.name;
+};
 getWeatherData();
-
-//`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
